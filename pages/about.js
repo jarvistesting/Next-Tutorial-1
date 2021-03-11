91930a6d57ffc97f77ce9d218808ce582f6dace1
jarvistesting <ravibca199600@gmail.com>
2021-03-11 14:43:31 +0530

    first commit

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Head from 'next/head'
+
+function about() {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name='keywords' content='ninjas' />
+            </Head>
+            <h1>About</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        </>
+    )
+}
+
+export default about
